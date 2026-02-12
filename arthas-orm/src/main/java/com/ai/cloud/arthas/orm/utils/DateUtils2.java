d033d5be1127c581026e2f348853b2e8f4d22fd0
Yjue <liuyimm0823@gmail.com>
2026-02-12 11:46:52 +0800

    feat: 日期转换工具类

---
+++ arthas-orm/src/main/java/com/ai/cloud/arthas/orm/utils/DateUtils2.java
@@ -0,0 +1,8 @@
+package com.ai.cloud.arthas.orm.utils;
+
+/**
+ * @author <a href="mailto:liuyi304@jd.com">liuyi304</a>
+ * @date 2026-02-11 18:47
+ * @description DateUtils2
+ */public class DateUtils2 {
+}
