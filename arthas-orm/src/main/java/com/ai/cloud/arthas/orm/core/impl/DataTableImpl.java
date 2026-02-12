b4da7e2daf3b39bdbb970dae232b651513cb139e
Yjue <liuyimm0823@gmail.com>
2026-02-12 13:56:26 +0800

    feat: dao接口框架

--- arthas-orm/src/main/java/com/ai/cloud/arthas/orm/core/impl/DataTableImpl.java
+++ arthas-orm/src/main/java/com/ai/cloud/arthas/orm/core/impl/DataTableImpl.java
@@ -1,9 +1,22 @@
 package com.ai.cloud.arthas.orm.core.impl;
 
+import com.ai.cloud.arthas.orm.base.TableDO;
+import com.ai.cloud.arthas.orm.core.DataTable;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * @author <a href="mailto:liuyi304@jd.com">liuyi304</a>
  * @date 2026-02-10 16:13
  * @description DataTableImpl
  */
-public class DataTableImpl {
+public class DataTableImpl <KEY, ENTITY extends TableDO<KEY>> implements DataTable<KEY, ENTITY> {
+
+    protected JdbcTemplate jdbcTemplate;
+
+    public DataTableImpl() {}
+
+    @Override
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+
+    }
 }
