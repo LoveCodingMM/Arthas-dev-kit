d29e0912625ec99df25c8bbca9b200896c9158e3
Yjue <liuyimm0823@gmail.com>
2026-02-13 14:45:38 +0800

    feat: where子查询Matches实现

--- arthas-orm/src/main/java/com/ai/cloud/arthas/orm/sql/Operate.java
+++ arthas-orm/src/main/java/com/ai/cloud/arthas/orm/sql/Operate.java
@@ -1,5 +1,7 @@
 package com.ai.cloud.arthas.orm.sql;
 
+import com.ai.cloud.arthas.orm.sql.where.operator.Operator;
+
 /**
  * @author <a href="mailto:liuyi304@jd.com">liuyi304</a>
  * @date 2026-02-12 14:26
@@ -17,5 +19,5 @@ public interface Operate extends SqlAble {
      * 获取操作符
      * @return 操作符对象
      */
-    Operate getOperator();
+    Operator getOperator();
 }
--- arthas-orm/src/main/java/com/ai/cloud/arthas/orm/sql/where/Matches.java
+++ arthas-orm/src/main/java/com/ai/cloud/arthas/orm/sql/where/Matches.java
@@ -1,6 +1,15 @@
 package com.ai.cloud.arthas.orm.sql.where;
 
 import com.ai.cloud.arthas.orm.sql.Operate;
+import com.ai.cloud.arthas.orm.sql.SqlAble;
+import com.ai.cloud.arthas.orm.sql.where.operator.FindInSetOperator;
+import com.ai.cloud.arthas.orm.sql.where.operator.MatchOperator;
+import com.ai.cloud.arthas.orm.sql.where.operator.NotMatchOperator;
+import com.ai.cloud.arthas.orm.sql.where.operator.Operator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author <a href="mailto:liuyi304@jd.com">liuyi304</a>
@@ -8,6 +17,92 @@ import com.ai.cloud.arthas.orm.sql.Operate;
  * @description Matches
  */
 public class Matches implements Operate {
+    private final static String AND = " and";
+    private final static String OR = " or";
 
+    private final String conj;
+    private final List<Operate> operates = new ArrayList<>(2);
+    private Matches(String conj) {
+        this.conj = conj;
+    }
 
+    /**
+     * @return Matches对象，空匹配集合
+     */
+    public static Matches empty() {
+        return and();
+    }
+
+    /**
+     * @return Matches对象，以and为连接符的匹配集
+     */
+    public static Matches and() {
+        return new Matches(AND);
+    }
+
+    /**
+     * @return Matches对象，以or为连接符的匹配集
+     */
+    public static Matches or() {
+        return new Matches(OR);
+    }
+
+    public Matches match(String column, Object value) {
+        operates.add(new Match(column, new MatchOperator(value)));
+        return this;
+    }
+
+    public Matches one(String column, Object value) {
+        return Matches.and()
+                      .match(column, value);
+    }
+
+    public Matches ne(String column, Object value) {
+        operates.add(new Match(column, new NotMatchOperator(value)));
+        return this;
+    }
+
+    public Matches findInSet(String column, String value) {
+        operates.add(new Match(column, new FindInSetOperator(value)));
+        return this;
+    }
+
+    @Override
+    public String getPreparedSql() {
+        if (isEmpty()) {
+            return "";
+        }
+        return operates.stream()
+                .map( operate -> {
+                    if (operate instanceof Matches) {
+                        return " ( " + operate.getPreparedSql() + " )";
+                    } else {
+                        return operate.getPreparedSql();
+                    }
+                })
+                .collect(Collectors.joining(conj));
+    }
+
+    @Override
+    public List<Object> getParams() {
+        List<Object> params = new ArrayList<>(operates.size() * 2);
+        for (SqlAble match : operates) {
+            params.addAll(match.getParams());
+        }
+        return params;
+    }
+
+    public boolean isEmpty() {
+        return operates.isEmpty();
+    }
+
+    @Override
+    public String getColumn() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Operator getOperator() {
+        throw new UnsupportedOperationException();
+    }
 }
