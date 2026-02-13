5e488298b4d690a915583f1695d36d73e2b35109
Yjue <liuyimm0823@gmail.com>
2026-02-13 11:17:18 +0800

    feat: sql接口

--- arthas-orm/src/main/java/com/ai/cloud/arthas/orm/sql/Operate.java
+++ arthas-orm/src/main/java/com/ai/cloud/arthas/orm/sql/Operate.java
@@ -4,5 +4,18 @@ package com.ai.cloud.arthas.orm.sql;
  * @author <a href="mailto:liuyi304@jd.com">liuyi304</a>
  * @date 2026-02-12 14:26
  * @description Operate
- */public interface Operate {
+ */
+public interface Operate extends SqlAble {
+
+    /**
+     * 获取操作列名
+     * @return string
+     */
+    String getColumn();
+
+    /**
+     * 获取操作符
+     * @return 操作符对象
+     */
+    Operate getOperator();
 }
--- arthas-orm/src/main/java/com/ai/cloud/arthas/orm/sql/SqlAble.java
+++ arthas-orm/src/main/java/com/ai/cloud/arthas/orm/sql/SqlAble.java
@@ -1,8 +1,23 @@
 package com.ai.cloud.arthas.orm.sql;
 
+import java.util.List;
+
 /**
  * @author <a href="mailto:liuyi304@jd.com">liuyi304</a>
  * @date 2026-02-12 14:21
  * @description SqlAble
- */public interface SqlAble {
+ */
+public interface SqlAble {
+
+    /**
+     * 生成sql
+     * @return sql string
+     */
+    String getPreparedSql();
+
+    /**
+     * 对应参数
+     * @return 参数列表
+     */
+    List<Object> getParams();
 }
