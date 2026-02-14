a10958700d5b7a647a7e977520149a5de2ba056f
Yjue <liuyimm0823@gmail.com>
2026-02-15 00:27:44 +0800

    feat: preparedUpdate占位符

--- arthas-orm/src/main/java/com/ai/cloud/arthas/orm/sql/update/PreparedUpdate.java
+++ arthas-orm/src/main/java/com/ai/cloud/arthas/orm/sql/update/PreparedUpdate.java
@@ -2,6 +2,8 @@ package com.ai.cloud.arthas.orm.sql.update;
 
 import com.ai.cloud.arthas.orm.sql.SqlAble;
 
+import java.util.List;
+
 /**
  * @author <a href="mailto:liuyi304@jd.com">liuyi304</a>
  * @date 2026-02-14 23:32
@@ -9,5 +11,14 @@ import com.ai.cloud.arthas.orm.sql.SqlAble;
  */
 public class PreparedUpdate implements SqlAble {
 
+    // todo: 该部分暂时作为占位符，第一版先实现核心功能。
+    @Override
+    public String getPreparedSql() {
+        return null;
+    }
 
+    @Override
+    public List<Object> getParams() {
+        return null;
+    }
 }
